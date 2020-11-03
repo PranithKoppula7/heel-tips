4cdf8c8f5ddabf8e33bd8fd7484d5d2707ac8117
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2020-11-03 15:36:07 -0500

    added backedn folder

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config')
+const authRoute = require('./routes/auth');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('we are home');
+});
+
+mongoose.connect(process.env.DB_CONNECTION, 
+{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to db')
+})
+
+
+app.listen(3000);
+
+app.use('/api/users', authRoute);
